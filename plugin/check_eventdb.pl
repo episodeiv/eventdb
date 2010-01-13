0fc6cb53eb16b923ced145d07d6fe0ad5c5dd85b
Birger Schmidt <bschmidt@ip-109-85-140-59.web.vodafone.de>
2010-01-13 08:41:24 +0100

    check_eventdb.pl - added ok trap capability, added result link into eventdb

--- plugin/check_eventdb.pl
+++ plugin/check_eventdb.pl
@@ -1,11 +1,12 @@
 #! /usr/bin/perl -w
 #
-# check_syslog-ng.pl - nagios plugin
+# check_eventdb.pl - nagios plugin
 #
 # $Id: check_eventdb.pl 69 2009-10-26 12:31:45Z wpreston $
 #
 # Copyright (C) 2007 Gerd Mueller / Netways GmbH
-# Copyright (C) 2008,20097 NETWAYS GmbH
+# Copyright (C) 2008,2009 NETWAYS GmbH
+# with additions by Rainer Brinkmoeller
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -51,6 +52,9 @@ use vars qw(
   $opt_label
   $opt_type
   $opt_facility
+  $opt_url
+  $opt_maxage
+  $opt_resetregexp
   $opt_program
   $opt_perfdata
   $dbh
@@ -58,10 +62,12 @@ use vars qw(
   $result
   $update
   $matches
+  $message
   $last
   $startfrom
   @sql
   @params
+  @urlparams
   $PROGNAME
   $VERSION
 );
@@ -87,6 +93,7 @@ $opt_priority = "";
 $opt_label    = "";
 $opt_type     = "";
 $opt_facility = "";
+$opt_resetregexp     = "";
 $opt_program = "";
 $opt_perfdata = "";
 $startfrom    = "0";
@@ -106,6 +113,8 @@ GetOptions(
 	"facility=s"   => \$opt_facility,
 	"priority=s"   => \$opt_priority,
 	"p=s"          => \$opt_priority,
+	"resetregexp=s"=> \$opt_resetregexp,
+	"r=s"          => \$opt_resetregexp,
 	"program=s"    => \$opt_program,
 	"P=s"          => \$opt_program,
 	"help"         => \$opt_help,
@@ -119,11 +128,32 @@ GetOptions(
 	"c=i"          => \$opt_critical,
 	"warning=i"    => \$opt_warning,
 	"w=i"          => \$opt_warning,
+	"url=s"        => \$opt_url,
+	"u=s"          => \$opt_url,
+	"maxage=s"     => \$opt_maxage,
 	"perfdata=s"   => \$opt_perfdata,
 );
 
 if ( !$opt_critical || !$opt_warning || $opt_help ) {
 	print_usage();
+}
+
+# convert $opt_maxage, if defined
+if ($opt_maxage ne '') {
+	# first, make a timestamp
+	my $timestamp = time();
+
+	# then split integer from string
+	my $int = (split(/(d|h|m)/, $opt_maxage))[0];
+
+	# and subduct from timestamp
+	$timestamp = $timestamp - $int * 86400 if $opt_maxage =~ /d/;	# days
+	$timestamp = $timestamp - $int * 3600  if $opt_maxage =~ /h/;   # hours
+	$timestamp = $timestamp - $int * 60    if $opt_maxage =~ /m/;   # minutes
+
+	# format timestamp to mysql datetime
+	my ($sec,$min,$hour,$mday,$mon,$year) = (localtime($timestamp))[0,1,2,3,4,5];
+	$opt_maxage = sprintf("%02d-%02d-%02d %02d:%02d:%02d", $year+1900,$mon+1,$mday,$hour,$min,$sec);
 }
 
 # count from the last match
@@ -141,25 +171,28 @@ $dbh = DBI->connect( "DBI:mysql:database=$opt_db;host=$opt_dbhost",
 # Count matches
 push @sql, sprintf('SELECT COUNT(uid) as count, MAX(uid) as last from %1$s where uid>%2$d', $opt_dbtable, $startfrom);
 
-add_part('host', $opt_host)
+add_part('host', 1, $opt_host)
 	if ($opt_host ne '');
 
-add_part('message', $opt_msg)
+add_part('message', 0, $opt_msg)
 	if ($opt_msg ne '');
 
-add_part('priority', $opt_priority)
+add_part('priority', 1, $opt_priority)
 	if ($opt_priority ne '');
 
-add_part('type', $opt_type)
+add_part('type', 1, $opt_type)
 	if ($opt_type ne '');
 
-add_part('facility', $opt_facility)
+add_part('facility', 1, $opt_facility)
 	if ($opt_facility ne '');
 
-add_part('program', $opt_program)
+add_part('datetime', 1, $opt_maxage, ">=")
+	if ($opt_maxage ne '');
+
+add_part('program', 0, $opt_program)
 	if ($opt_program ne '');
 
-add_part('acknowledged', '0');
+add_part('acknowledged', 0, '0');
 
 
 # preparing the query
@@ -171,9 +204,11 @@ $result = $sth->execute(@params)
   || myexit( 'UNKNOWN', "Can't execute the query: ". $sth->errstr );
 
 $matches = 0;
+$message = "";
 if ($result) {
 	while(my @row = $sth->fetchrow_array()) {
 		($matches, $last) = @row;
+		$message = $row[0];
 	}
 
 	$sth->finish();
@@ -188,16 +223,40 @@ $dbh->disconnect()
 
 
 if ( $matches >= $opt_critical ) {
-	myexit( 'CRITICAL', $matches . " matches found!", "matches=$matches count=$last"."c" );
+    if ( $opt_resetregexp eq '' ) {
+    	myexit( 'CRITICAL', $message, "matches=$matches count=$last"."c" );
+    }
+    elsif ( $message =~ m/($opt_resetregexp)/ ) {
+    	myexit( 'OK', "Matches found already reseted.", "matches=$matches count=$last"."c" );
+    }
+    else {
+    	myexit( 'CRITICAL', $message, "matches=$matches count=$last"."c" );
+    }
+	#myexit( 'CRITICAL', $matches . " matches found!", "matches=$matches count=$last"."c" );
 }
 elsif ( $matches >= $opt_warning ) {
-	myexit( 'WARNING', $matches . " matches found.", "matches=$matches count=$last"."c" );
+    if ( $opt_resetregexp eq '' ) {
+    	myexit( 'WARNING', $message, "matches=$matches count=$last"."c" );
+    }
+    elsif ( $message =~ m/($opt_resetregexp)/ ) {
+    	myexit( 'OK', "Matches found already reseted.", "matches=$matches count=$last"."c" );
+    }
+    else {
+    	myexit( 'WARNING', $message, "matches=$matches count=$last"."c" );
+    }
+	#myexit( 'WARNING', $matches . " matches found.", "matches=$matches count=$last"."c" );
 }
 else {
 	myexit( 'OK', $matches . " matches found.", "matches=$matches count=$last"."c" );
 }
 
 myexit( 'UNKNOWN', 'Default exit' );
+
+sub urlencode {
+	my $str = "@_";
+	$str =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
+	return $str;
+}
 
 sub myexit {
 	my $time;
@@ -209,7 +268,14 @@ sub myexit {
 	  ( 'UNKNOWN' => '-1', 'OK' => '0', 'WARNING' => '1', 'CRITICAL' => '2' );
 
 	my $out = undef;
-	$out .= sprintf('%1$s: %2$s %3$s', $status, $opt_label, $text);
+	if ($opt_url) {
+		$out .= sprintf('%1$s: %2$s %3$s', $status, $opt_label, 
+		'<a href="' . $opt_url . '?' . join('&', @urlparams) . '">' . $text . '</a>');
+	} else {	
+		$out .= sprintf('%1$s: %2$s %3$s', $status, $opt_label, $text);
+	}	
+	$out .= "\nmessage filter: $opt_msg";
+	$out .= "\nreset regexp: $opt_resetregexp";
 	$out .= sprintf('|%1$s', $perfdata) if ($perfdata);
 	$out .= chr(10);
 
@@ -219,7 +285,7 @@ sub myexit {
 }
 
 sub add_part {
-	my ($field, $value, $op, $agg) = @_;
+	my ($field, $is_array, $value, $op, $agg) = @_;
 
 	$agg = 'AND' if (!defined $agg);
 	$op = '=' if (!defined $op);
@@ -234,6 +300,8 @@ sub add_part {
 		$agg, $field, $op
 	);
 
+	if ($is_array == 1) {$field .= '[]'}
+	push @urlparams, "$field=" . urlencode($value);
 	push @params, $value;
 
 	return 1;
@@ -241,7 +309,7 @@ sub add_part {
 
 sub print_usage {
 	print <<EOU;
-    Usage: $PROGNAME [ -H host ] [ -p priority ] [ -t type ] [ -m msg ] [ -P program ] [ --db db ] [ --dbtabe dbtable ] [ --dbuser dbuser ] [ --dbpassword dbpassword ] [ --dbhost dbhost ] [ -l label ] [ --perfdata '\$SERVICEPERFDATA\$' ]  -w warn -c crit
+    Usage: $PROGNAME [ -H host ] [ -p priority ] [ -t type ] [ -m msg ] [ -P program ] [ --db db ] [ --dbtabe dbtable ] [ --dbuser dbuser ] [ --dbpassword dbpassword ] [ --dbhost dbhost ] [ -l label ] [ --perfdata '\$SERVICEPERFDATA\$' ] [ --maxage STRING ] [ -r resetregexp ] -w warn -c crit
 
     Options:
 
@@ -263,12 +331,16 @@ sub print_usage {
         Databaseuser (default: none)
     --dbpassword STRING
         Databasepassword (default: none)
-    --dbpassword STRING
-        Databasepassword (default: none)
     --dbhost STRING
         Databaseserver (default: localhost)
     -l --label STRING
         label for plugin output
+    -u --url STRING
+        URL for EventDB link in plugin output
+    --maxage STRING
+        Max age of EventDB entry (eg. 1m, 2h, 3d)
+    -r --resetregexp STRING
+        Regular Expression for message entry in eventdb to change each state back to OK.
     --perfdata STRING
         performance data from the last check (e.g. \$SERVICEPERFDATA\$)
     -w --warning INTEGER
@@ -280,4 +352,19 @@ EOU
 
 	myexit( "UNKNOWN", $PROGNAME );
 }
+
+
+# define service{
+#         use                             generic-service
+#         host_name                       localhost
+#         service_description             eventdb
+#         # critical for all status other than 4.
+#         check_command                   check_eventdb!1!1!%Interesting Status:%!-r Status: 4
+#         }
+# 'check_eventdb' command definition
+# 
+# define command{
+#         command_name            check_eventdb
+#         command_line            $USER1$/contrib/check_eventdb.pl --dbuser eventdbrw --dbpassword eventdbrw --url "/nagios/eventdb/index.php" -w $ARG1$ -c $ARG2$ -m "$ARG3$" "$ARG4$"
+# }
 
