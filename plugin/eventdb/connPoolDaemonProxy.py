b809677b1cc8b7653530ef01aa04e522f0640330
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-01-13 19:59:34 +0100

    * Restructure, added setup.py (Part 1)

---
+++ plugin/eventdb/connPoolDaemonProxy.py
@@ -0,0 +1,58 @@
+"""
+Proxy interface to allow using connPoolDaemon the same way as DBhandler
+
+"""
+from connPoolDaemon import *
+
+DEFAULT_LOG = DAEMON_DEFAULT_LOG
+# defines the behaviour when (detectable) race conditions occur
+CONCURRENCY_BEHAVIOURS = DAEMON_CONCURRENCY_BEHAVIOURS
+
+__all__ = ['ConnPoolDaemonProxy','CONCURRENCY_BEHAVIOURS','DEFAULT_LOG']
+class ConnPoolDaemonProxy(object):
+
+    def __init__(self,pid,logFile = DAEMON_DEFAULT_LOG, behaviour = "Aggressive", timeout = 5):
+        self.__cursor = False
+        self.__pid = pid
+        self.__logFile = logFile
+        self.__behaviour = behaviour
+        self.__timeout = timeout
+        
+
+    def execute(self,query):
+        result = self.__daemon.request(
+            {
+                "query" : query,
+                "options" : {
+                    "driver" : self.__driver,
+                    "host" : self.__host,
+                    "port" : self.__port,
+                    "database" : self.__database,
+                    "user" : self.__user,
+                    "password" : self.__password
+                }
+            }
+        )
+        if result == False:
+            raise Exception("Requesting daemon failed")
+        return result
+
+    def connect(self,driver,host="localhost",user=None,password=None,database=None,port=None,poolsize=3):
+        self.__daemon = ConnPoolDaemon(
+            self.__pid,
+            True,
+            self.__logFile,
+            self.__behaviour,
+            self.__timeout
+        )
+        # Just store those values, they are needed in the execute method
+        self.__driver = driver;
+        self.__host = host
+        self.__port = port
+        self.__database = database
+        self.__user = user
+        self.__password = password
+        
+
+        if(self.__daemon.connect() == False):
+            raise Exception("Couldn't connect to daemon")
---
+++ plugin/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name="EventDB_check_plugin",
+      version="2.0.4",
+      author="Jannis Mosshammer",
+      author_email="jannis.mosshammer@netways.de",
+      description=("Icinga/Nagios check plugin for the eventdb "),
+      license="GPLv2",
+      keywords="eventdb nagios icinga monitoring netways",
+      url="http://www.netways.org/projects/eventdb",
+      packages=['eventdb','eventdb.bin'],
+      requires=['sqlalchemy (>=0.6.3)'],
+      long_description=read('../doc/README'),
+      entry_points={
+        'console_scripts': ['check_eventdb = eventdb.bin.check_eventdb:main']
+      },
+
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: System :: Monitoring",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
+      ]
+)
