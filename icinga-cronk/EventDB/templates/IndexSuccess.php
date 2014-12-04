41c19477fd649aa8aee06ae7127998d992bf0be5
Eric Lippmann <eric.lippmann@netways.de>
2014-12-04 13:09:30 +0100

    Fix code compliance issues in the index success view sript

--- icinga-cronk/EventDB/templates/IndexSuccess.php
+++ icinga-cronk/EventDB/templates/IndexSuccess.php
@@ -1,17 +1,16 @@
 <script type="text/javascript">
-// This is the init method called when the cronk environment is ready
 Cronk.util.initEnvironment("<?php echo $rd->getParameter('parentid'); ?>", function() {
     var cfg = {
-		CE : this,
-    	parentCmp : this.getParent(),
-    	eventUrl : '<?php echo $ro->gen("modules.eventdb.events.list"); ?>',
-   		commentUrl : '<?php echo $ro->gen("modules.eventdb.events.event.comments.list"); ?>',
-		commentAddUrl : '<?php echo $ro->gen("modules.eventdb.events.comments.add"); ?>',
-		userName : '<?php echo $us->getNsmUser()->user_name; ?>',
-        showCopyPaste : '<?php echo AgaviConfig::get("modules.eventdb.showCopyPaste",false); ?>',
+        CE: this,
+        parentCmp: this.getParent(),
+        eventUrl: '<?php echo $ro->gen("modules.eventdb.events.list"); ?>',
+        commentUrl: '<?php echo $ro->gen("modules.eventdb.events.event.comments.list"); ?>',
+        commentAddUrl: '<?php echo $ro->gen("modules.eventdb.events.comments.add"); ?>',
+        userName: '<?php echo $us->getNsmUser()->user_name; ?>',
+        showCopyPaste: '<?php echo AgaviConfig::get("modules.eventdb.showCopyPaste",false); ?>',
         additionalFields:  <?php echo json_encode($rd->getParameter('additionalFields', array())); ?>,
         mailMeUrl: '<?php echo $ro->gen('modules.eventdb.events.event.mailme'); ?>'
-	};
-	Cronk.EventDB.MainView(cfg);
+    };
+    Cronk.EventDB.MainView(cfg);
 });
 </script>
