2460e0d8d18b99c0f7e2cdbc797cde0bada6c20b
Eric Lippmann <eric.lippmann@netways.de>
2015-09-11 14:00:52 +0200

    Fix code style in ListSuccessView

--- icinga-cronk/EventDB/views/Events/ListSuccessView.class.php
+++ icinga-cronk/EventDB/views/Events/ListSuccessView.class.php
@@ -2,42 +2,42 @@
 
 class EventDB_Events_ListSuccessView extends EventDBBaseView {
 
-	public function executeHtml(AgaviRequestDataHolder $rd) {
-		$this->setupHtml($rd);
+    public function executeHtml(AgaviRequestDataHolder $rd) {
+        $this->setupHtml($rd);
 
-		$this->setAttribute('_title', 'EventDB.Events.List');
-	}
+        $this->setAttribute('_title', 'EventDB.Events.List');
+    }
 
-	public function executeJson(AgaviRequestDataHolder $rd) {
-		$edb = $this->getContext()->getModel('EventDB', 'EventDB');
-		$qfilter = $rd->getParameter("hostQuickFilter",array());
+    public function executeJson(AgaviRequestDataHolder $rd) {
+        $edb = $this->getContext()->getModel('EventDB', 'EventDB');
+        $qfilter = $rd->getParameter('hostQuickFilter',array());
 
-		$filter =  $rd->getParameter('filter', array());
-		$filter[] = $qfilter;
+        $filter =  $rd->getParameter('filter', array());
+        $filter[] = $qfilter;
 
-		if(empty($filter))
-			$filter = false;
+        if (empty($filter)) {
+            $filter = false;
+        }
 
 
-		$db = $edb->getEvents(array(),
-				$rd->getParameter('offset', 0),
-				$rd->getParameter('limit', false),
-				array(
-                    'simple' => $rd->getParameter('simple',false) ? true : false,
-					'order_by' => $rd->getParameter('order_by',false),
-					'dir' => $rd->getParameter('dir','DESC'),
-					'target' => $rd->getParameter('target', 'EventDbEvent'),
-				    'columns' => $rd->getParameter('columns', array('*')),
-				    'group_by' => $rd->getParameter('group_by', false),
-				    'filter' => $filter,
-                    'group_leader' => $rd->getParameter('group_leader',false)
-					//'count' => $rd->getParameter('count','id')
-				)
-			);
+        $db = $edb->getEvents(
+            array(),
+            $rd->getParameter('offset', 0),
+            $rd->getParameter('limit', false),
+            array(
+                'simple'        => $rd->getParameter('simple',false) ? true : false,
+                'order_by'      => $rd->getParameter('order_by',false),
+                'dir'           => $rd->getParameter('dir','DESC'),
+                'target'        => $rd->getParameter('target', 'EventDbEvent'),
+                'columns'       => $rd->getParameter('columns', array('*')),
+                'group_by'      => $rd->getParameter('group_by', false),
+                'filter'        => $filter,
+                'group_leader'  => $rd->getParameter('group_leader',false)
+            )
+        );
 
-		return json_encode(array(
-			'events' => $db["values"]
-//			'count' => $db["count"]
-		));
-	}
+        return json_encode(array(
+            'events' => $db['values']
+        ));
+    }
 }
